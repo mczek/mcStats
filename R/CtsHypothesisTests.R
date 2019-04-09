b95c6564ba54cd93a3f09a7700dbfc01852a10d1
Michael <mczekanski1@gmail.com>
2019-04-08 21:18:23 -0400

    introduce showChiSq.Test and testing code

--- R/CtsHypothesisTests.R
+++ R/CtsHypothesisTests.R
@@ -22,8 +22,6 @@ mcDNorm <- function(x, mean = 0, sd = 1, log = FALSE, ...){
 }
 
 
-
-
 #' @title Used to shade in a PDF
 #' @description Returns density with extreme event region having NAs
 #'
@@ -45,6 +43,7 @@ shadePDFCts <- function(x, fun, testStat, ...){
 #'
 #' @param testStat test statistic
 #' @param densFun function that computes appropriate density
+#' @param xlims  x limits of the graph to be used. This is passed to ggplot
 #' @param degFree degrees of freedom when only one is needed. This gets passed into densFun
 #' @param ... extra arguments passed to density function
 #'
@@ -58,16 +57,16 @@ shadePDFCts <- function(x, fun, testStat, ...){
 #' showT.Test(x)
 #'
 #' @export
-showXtremeEventsCts <- function(testStat, densFun, degFree, ...){
+showXtremeEventsCts <- function(testStat, densFun, degFree, xlims, ...){
   # print(...)
-  xlimVal <- max(abs(testStat) + 1, 3)
+
   fakeData <- data.frame(x = c(-testStat, testStat),
                          Statistic = c("Equally Extreme Event", "Your Test Statistic"))
   # extraArgs <- ...
   plt <- ggplot(fakeData, aes_(x = ~ x)) +
     stat_function(fun = densFun,
                   args = list(df = degFree)) +
-    stat_function(data = data.frame(x = c(-xlimVal, xlimVal)),
+    stat_function(data = data.frame(x = xlims),
                   mapping = aes_(x = ~ x),
                   fun = shadePDFCts,
                   geom = "area",
@@ -79,6 +78,7 @@ showXtremeEventsCts <- function(testStat, densFun, degFree, ...){
     geom_vline(aes_(xintercept = ~ x,
                     color = ~ Statistic),
                size = 3) +
+    xlim(xlims) +
     scale_color_colorblind() +
     theme_bw() +
     labs(x = "Test Statistic",
@@ -89,10 +89,12 @@ showXtremeEventsCts <- function(testStat, densFun, degFree, ...){
 }
 
 #' @title Conduct z-test
-#' @description Runs z-test and outputs graph for interpretation
+#' @description Runs z-test and outputs graph for interpretation using stats::t.test
+#'
 #' @param group1 continuous data to test
 #' @param group2 optional: second group to include for two sample t-test
 #' @param mu optional: mean to test against for one-sample t-test
+#' @param paired boolean, if TRUE perform matched pairs t-test
 #' @param verbose default is 1 which will create a graph. To turn this off use verbose = 0.
 #'
 #' @return results of call to t.test
@@ -104,15 +106,49 @@ showXtremeEventsCts <- function(testStat, densFun, degFree, ...){
 #' showT.Test(x)
 #'
 #' @export
-showT.Test <- function(group1, group2 = NULL, mu = 0, verbose = 1){
+showT.Test <- function(group1, group2 = NULL, mu = 0, paired = FALSE, verbose = 1){
   testResult <- t.test(group1, group2, mu = mu)
   testStat <- testResult$statistic
   degFree <- testResult$parameter
+  xlimVal <- max(abs(testStat) + 1, 3)
 
   if(verbose > 0){
     showXtremeEventsCts(testStat = testStat,
                      densFun = dt,
+                     xlims = c(-xlimVal, xlimVal),
                      degFree = degFree)
   }
   return(testResult)
+}
+
+#' Show Chi-Square Test
+#' @description show results of a chi-square test visually using \link[stats]{chisq.test}
+#'
+#' @param x a numeric vector or matrix. x and can also be factors
+#' @param y a numeric vector
+#' @param p a vector of proabilities the same length as x. Used for goodness-of-fit tests. Must be a valid distribution
+#' @param simulate.p.value boolean, if TRUE use simulation to estimate p-value
+#' @param nreps if simulate.p.value = TRUE number of simulations to complete
+#' @param verbose level of visual output, 0 = silent
+#'
+#' @return results of \link[stats]{chisq.test} call
+#' @export
+#'
+#' @examples
+#' showChiSq.Test(x = c(1,2,1), y= c(1,2,2))
+showChiSq.Test <- function(x, y = NULL, p = rep(1/length(x), length(x)),
+                           simulate.p.value = FALSE, nreps = 2000, verbose = 1){
+  testResult <- chisq.test(x, y, p, simulate.p.value, B = nreps)
+  testStat <- testResult$statistic
+  degFree <- testResult$parameter
+  xlims <- c(0, max(qchisq(0.99, degFree), testStat + 1))
+
+  if(verbose > 0){
+    showXtremeEventsCts(testStat = testStat,
+                        densFun = dchisq,
+                        xlims = xlims,
+                        degFree = degFree)
+  }
+  return(testResult)
+
 }
