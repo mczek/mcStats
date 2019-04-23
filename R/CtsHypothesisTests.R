ca3c1465c7a16bcd8123d3309f9e7f4f55643154
Michael <mczekanski1@gmail.com>
2019-04-23 16:07:38 -0400

    update showMosaicPlot

--- R/CtsHypothesisTests.R
+++ R/CtsHypothesisTests.R
@@ -184,13 +184,13 @@ showT.Test <- function(group1, group2 = NULL, mu = 0, paired = FALSE, verbose = 
 #' @param x must be a matrix with each row and column labelled
 #' @import magrittr
 #' @importFrom tidyr expand
-#' @importFrom  dplyr right_join filter_ mutate_
+#' @importFrom  dplyr right_join filter_
 #' @return mosaic plot showing observed proportions, colored by residuals from chi-sq. test
 #' @export
 #' @examples
 #' x <- matrix(runif(9,5,100), ncol = 3, dimnames = list(c("Yes1", "No1", "Maybe1"),
 #' c("Yes2", "No2", "Maybe2")))
-#' mosaicplot(x)
+#' showMosaicPlot(x)
 showMosaicPlot <- function(x){
   if(is.null(rownames(x)) | is.null(colnames(x))){
     warning("Row and Columns must be named.")
@@ -249,12 +249,14 @@ showMosaicPlot <- function(x){
   resData <- right_join(df, resData)
 
   labelDF1 <- resData %>%
-    filter_(~Var2 == colnames(x)[1]) %>%
-    mutate_(yMean = (~yStart + ~yEnd)/2)
+    filter_(~Var2 == colnames(x)[1])
+
+  labelDF1$yMean <- (labelDF1$yStart + labelDF1$yEnd)/2
 
   labelDF2 <- resData %>%
-    filter_(~Var1 == rownames(x)[1]) %>%
-    mutate_(xMean = (~xStart + ~xEnd)/2)
+    filter_(~Var1 == rownames(x)[1])
+
+  labelDF2$xMean <- (labelDF2$xStart + labelDF2$xEnd)/2
 
   plt <- resData %>%
     ggplot() +
